97e82f937900500fb7e80228f098dce84b091844
mahdieh-00 <mahdirahmani1377@outlook.com>
2024-03-07 20:21:09 +0330

    15th-commit

--- main.js/user.js
+++ main.js/user.js
@@ -2,3 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems,{});
   });
+
+
+
+
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
