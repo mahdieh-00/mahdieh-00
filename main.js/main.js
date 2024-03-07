65116b06bc4f99f1b245e913dc18558af5a4f2e9
mahdieh-00 <mahdirahmani1377@outlook.com>
2024-03-07 15:18:25 +0330

    14th-commit

--- main.js/main.js
+++ main.js/main.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+
+    const addUserButton = document.getElementById('add-user-button')
+    addUserButton.addEventListener('click', ()=>{
+      document.querySelector('.add-user-modal').classList.add('show')
+      document.querySelector('.add-user-modal-back').classList.remove('dis-none')
+    })
+
+
+    document.querySelector('add-user-modal-back').addEventListener('click' , (e)=>{
+      document.querySelector('.add-user-modal').classList.remove('show')
+      e.target.classList.add('dis-none')
+    })
   });
